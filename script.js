2f9fac941e485763dd193d4c4493da67408250d9
m0jojoj00 <yarkiyq@gmail.com>
2026-01-28 02:43:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('site loaded')
+const TOKEN = "8385634017:AAHCnvVkUe6v7MH8EGSd4wj5y35eaZoeCKg";
+const CHAT_ID = "-1003869892653";
+
+document.querySelector("form")?.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const name = document.querySelector("[name='name']").value;
+  const company = document.querySelector("[name='company']").value;
+  const contact = document.querySelector("[name='contact']").value;
+  const goal = document.querySelector("[name='goal']").value;
+  const description = document.querySelector("[name='description']").value;
+
+  const message = `
+🚀 Новая заявка
+
+👤 Имя: ${name}
+🏢 Компания: ${company}
+📞 Контакт: ${contact}
+🎯 Цель: ${goal}
+
+📝 Описание:
+${description}
+`;
+
+  await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({
+      chat_id: CHAT_ID,
+      text: message
+    })
+  });
+
+  alert("Заявка отправлена. Мы свяжемся с вами.");
+  e.target.reset();
+});
